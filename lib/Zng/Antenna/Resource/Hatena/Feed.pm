9812de726621324c95910f306511b7eb4e34485a
HIRANO Takahito <hiranotaka@zng.info>
2011-01-30 15:29:44 +0900

    Add the Hatena resource.
    Signed-off-by: HIRANO Takahito <hiranotaka@zng.info>

---
+++ lib/Zng/Antenna/Resource/Hatena/Feed.pm
@@ -0,0 +1,74 @@
+package Zng::Antenna::Resource::Hatena::Feed;
+
+use strict;
+use vars qw{@ISA};
+use Digest::MD5 qw{md5};
+use Zng::Antenna::Resource::Base::Feed;
+use Zng::Antenna::Resource::Hatena::Thread;
+
+@ISA = qw{Zng::Antenna::Resource::Base::Feed};
+
+sub new ( $$ ) {
+    my $class = shift;
+    my $data = shift;
+
+    my $user_id = $data->{user_id};
+    $data->{url} = "http://d.hatena.ne.jp/$user_id/rss";
+
+    my $self = bless {
+	data => $data,
+    }, $class;
+    return $self;
+}
+
+sub id ( $ ) {
+    my $self = shift;
+    my $data = $self->{data};
+
+    my $id = "\x8e\xd8\xe8\xc1\x01\x76\x08\x90\xf8\xcf\x7e\x2d\x74\x28\xc6\x69";
+    $id = md5($id . $data->{user_id});
+    return $id;
+}
+
+sub title ( $ ) {
+    my $self = shift;
+    return $self->{data}->{title};
+}
+
+sub link ( $ ) {
+    my $self = shift;
+    my $user_id = $self->{data}->{user_id};
+    return "http://d.hatena.ne.jp/$user_id/";
+}
+
+sub mobile_link ( $ ) {
+    my $self = shift;
+    my $user_id = $self->{data}->{user_id};
+    return "http://d.hatena.ne.jp/$user_id/archivemobile";
+}
+
+sub threads ( $ ) {
+    my $self = shift;
+    my $threads_data = $self->{data}->{threads};
+    my $threads = [];
+    for my $thread_data (@$threads_data) {
+	my $thread = Zng::Antenna::Resource::Hatena::Thread->new($thread_data);
+	push @$threads, $thread;
+    }
+    return @$threads;
+}
+
+sub standalone ( $ ) {
+    my $self = shift;
+    return 0;
+}
+
+sub update ( $$ ) {
+    my $self = shift;
+    my $net = shift;
+
+    require Zng::Antenna::Resource::XML::Updater;
+    Zng::Antenna::Resource::XML::Updater::update($self->{data}, $net);
+}
+
+1;
---
+++ lib/Zng/Antenna/Resource/Hatena/Thread.pm
@@ -0,0 +1,86 @@
+package Zng::Antenna::Resource::Hatena::Thread;
+
+use strict;
+use vars qw{@ISA};
+use Digest::MD5 qw{md5};
+use Zng::Antenna::Resource::Base::Thread;
+use Zng::Antenna::Resource::Hatena::Feed;
+
+@ISA = qw{Zng::Antenna::Resource::Base::Thread};
+
+sub new ( $$ ) {
+    my $class = shift;
+    my $data = shift;
+
+    my $id = $data->{id};
+    my $user_id_re = quotemeta $data->{feed}->{user_id};
+    if ($id =~ /http:\/\/d.hatena.ne.jp\/$user_id_re\/([^#]+)#(.*)/) {
+	$data->{date} = int $1;
+	$data->{section} = int $2;
+    }
+
+    my $self = bless {
+	data => $data,
+    }, $class;
+
+    return $self;
+}
+
+sub id ( $ ) {
+    my $self = shift;
+    my $id = $self->feed->id;
+    $id = md5($id . $self->{data}->{id});
+    return $id;
+}
+
+sub title ( $ ) {
+    my $self = shift;
+    return $self->{data}->{title};
+}
+
+sub link ( $ ) {
+    my $self = shift;
+    my $data = $self->{data};
+    my $user_id = $data->{feed}->{user_id};
+    my $date = $data->{date};
+    my $section = $data->{section};
+    return "http://d.hatena.ne.jp/$user_id/$date#$section";
+}
+
+sub mobile_link ( $ ) {
+    my $self = shift;
+    my $data = $self->{data};
+    my $user_id = $data->{feed}->{user_id};
+    my $date = $data->{date};
+    my $section = $data->{section};
+    return "http://d.hatena.ne.jp/$user_id/mobile?date=$date;section=$section";
+}
+
+sub base_uri ( $ ) {
+    my $self = shift;
+    return $self->{data}->{base_uri};
+}
+
+sub text_content ( $ ) {
+    my $self = shift;
+    return $self->{data}->{text_content};
+}
+
+sub html_content ( $ ) {
+    my $self = shift;
+    return $self->{data}->{html_content};
+}
+
+sub updated ( $ ) {
+    my $self = shift;
+    return $self->{data}->{updated};
+}
+
+sub feed ( $ ) {
+    my $self = shift;
+    my $feed_data = $self->{data}->{feed};
+    my $feed = Zng::Antenna::Resource::Hatena::Feed->new($feed_data);
+    return $feed;
+}
+
+1;
