aaf80325c31c2bd03fe5bace77cbea9acba3a864
HIRANO Takahito <hiranotaka@zng.info>
2011-02-25 00:35:32 +0900

    Support standalone XMLs.
    Signed-off-by: HIRANO Takahito <hiranotaka@zng.info>

---
+++ lib/Zng/Antenna/Resource/XML/StandaloneFeed.pm
@@ -0,0 +1,58 @@
+package Zng::Antenna::Resource::XML::StandaloneFeed;
+
+use strict;
+use vars qw{@ISA};
+use Digest::MD5 qw{md5};
+use Zng::Antenna::Resource::Base::Feed;
+use Zng::Antenna::Resource::XML::StandaloneThread;
+
+@ISA = qw{Zng::Antenna::Resource::Base::Feed};
+
+sub new ( $$ ) {
+    my $class = shift;
+    my $data = shift;
+
+    my $self = bless {
+	data => $data,
+    }, $class;
+    return $self;
+}
+
+sub id ( $ ) {
+    my $self = shift;
+    my $data = $self->{data};
+
+    my $id = "\x3c\xcd\xbf\xc0\xed\x0f\x01\xe3\xc0\x5f\x66\x04\xc4\x67\x1d\xa2";
+    $id = md5($id . $data->{url});
+    return $id;
+}
+
+sub title ( $ ) {
+    my $self = shift;
+    return $self->{data}->{title};
+}
+
+sub link ( $ ) {
+    my $self = shift;
+    return $self->{data}->{link};
+}
+
+sub threads ( $ ) {
+    my $self = shift;
+    return Zng::Antenna::Resource::XML::StandaloneThread->new($self->{data});
+}
+
+sub standalone ( $ ) {
+    my $self = shift;
+    return 1;
+}
+
+sub update ( $$ ) {
+    my $self = shift;
+    my $net = shift;
+
+    require Zng::Antenna::Resource::XML::Updater;
+    Zng::Antenna::Resource::XML::Updater::update($self->{data}, $net);
+}
+
+1;
---
+++ lib/Zng/Antenna/Resource/XML/StandaloneThread.pm
@@ -0,0 +1,60 @@
+package Zng::Antenna::Resource::XML::StandaloneThread;
+
+use strict;
+use vars qw{@ISA};
+use CGI;
+use Digest::MD5 qw{md5};
+use Zng::Antenna::Resource::Base::Thread;
+use Zng::Antenna::Resource::XML::StandaloneFeed;
+
+@ISA = qw{Zng::Antenna::Resource::Base::Thread};
+
+sub new ( $$ ) {
+    my $class = shift;
+    my $data = shift;
+
+    my $self = bless {
+	data => $data,
+    }, $class;
+
+    return $self;
+}
+
+sub id ( $ ) {
+    my $self = shift;
+    return $self->feed->id;
+}
+
+sub title ( $ ) {
+    my $self = shift;
+    return $self->{data}->{title};
+}
+
+sub link ( $ ) {
+    my $self = shift;
+    return $self->{data}->{link};
+}
+
+sub content ( $ ) {
+    my $self = shift;
+    my $threads = $self->{data}->{threads};
+    @$threads > 0 or return undef;
+    my $thread = $threads->[0];
+    my $title = $thread->{title};
+    my $content = $thread->{content};
+    return "$title: $content";
+}
+
+sub updated ( $ ) {
+    my $self = shift;
+    my $threads = $self->{data}->{threads};
+    @$threads > 0 or return undef;
+    return $threads->[0]->{updated};
+}
+
+sub feed ( $ ) {
+    my $self = shift;
+    return Zng::Antenna::Resource::XML::StandaloneFeed->new($self->{data});
+}
+
+1;
